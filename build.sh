4d59090dca36f18f4bcc839859060e65355a97cb
xujiang <realjiangxu@gmail.com>
2020-06-23 22:42:12 +0800

    fix build scripts

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ mvn versions:set -DnewVersion=2.7.4.0.0
 
 if [ $mode == "all" ]; then
 	echo 'Run mode: ' $mode
-	mvn -B clean install package rpm:rpm -DnewVersion=2.7.4.0.0 -Dstack.distribution=HDP -DbuildNumber=631319b00937a8d04667d93714241d2a0cb17275 -DskipTests -DskipTests -Dpython.ver="python >= 2.6" -Drat.skip -Preplaceurl
+	mvn -B clean install package rpm:rpm -DnewVersion=2.7.4.0.0 -Dstack.distribution=HDP -DbuildNumber=631319b00937a8d04667d93714241d2a0cb17275 -DskipTests -Dpython.ver="python >= 2.6" -Drat.skip -Preplaceurl
 fi
 
 if [ $mode == "single" ]; then
